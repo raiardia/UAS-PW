b395848670f2dd8fc372c38c00b9d145cae9a5da
raiardia <ardiaanggreni@gmail.com>
2024-06-12 07:40:45 +0800

    Menambahkan halaman Blog dengan judul entri blog tentang sejarah anjing

--- blog-single1.php
+++ blog-single1.php
@@ -7,6 +7,35 @@
 
 </head>
 <body>
-    
+    <?php include "layout/header" ?>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	    	<a class="navbar-brand" href="index.php"><span class="flaticon-pawprint-1 mr-2"></span>DOGI FOR US</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+				<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="about.php" class="nav-link">About Us</a></li>
+				<li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+				<li class="nav-item active"><a href="blog.php" class="nav-link">Blog</a></li>
+				<li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+			</ul>
+	      </div>
+	    </div>
+	  </nav>
+<!-- END nav -->
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end">
+          <div class="col-md-9 ftco-animate pb-5">
+          	<p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span class="mr-2"><a href="blog.php">Blog <i class="ion-ios-arrow-forward"></i></a></span> <span>Blog Single <i class="ion-ios-arrow-forward"></i></span></p>
+            <h1 class="mb-0 bread">Sejarah Anjing: Sahabat Setia Manusia dari Zaman Prasejarah hingga Era Modern</h1>
+          </div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>
