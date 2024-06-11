8c28c4c3a8d512559f5cb0dec3705e90bc8f26d7
raiardia <ardiaanggreni@gmail.com>
2024-06-12 07:19:59 +0800

    Penambahan file header yang di-include menggunakan PHP.

--- about.php
+++ about.php
@@ -6,6 +6,24 @@
     <title>UAS PEMROGRAMAN WEB</title>
 </head>
 <body>
+    <?php include "layout/header" ?>
+	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	    	<a class="navbar-brand" href="index.php"><span class="flaticon-pawprint-1 mr-2"></span>DOGI FOR US</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+				<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+				<li class="nav-item active"><a href="about.php" class="nav-link">About Us</a></li>
+				<li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+				<li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+				<li class="nav-item"><a href="quiz.php" class="nav-link">Quiz</a></li>
+			</ul>
+	      </div>
+	    </div>
+	  </nav>
     
 </body>
 </html>
