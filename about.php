4c951039bf62701b9848a6391dc46fba1cbb2d9e
raiardia <ardiaanggreni@gmail.com>
2024-06-12 07:22:24 +0800

    Penambahan bagian hero untuk halaman "Tentang Kami".

--- about.php
+++ about.php
@@ -24,6 +24,18 @@
 	      </div>
 	    </div>
 	  </nav>
+
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end">
+          <div class="col-md-9 ftco-animate pb-5">
+          	<p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Home<i class="ion-ios-arrow-forward"></i></a></span> <span>Tentang Kami<i class="ion-ios-arrow-forward"></i></span></p>
+            <h1 class="mb-0 bread">DOGI FOR US</h1>
+          </div>
+        </div>
+      </div>
+    </section>
     
 </body>
 </html>
