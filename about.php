4fe685e8a8c34427d36a8fce6b65f9715b5c333f
raiardia <ardiaanggreni@gmail.com>
2024-06-12 07:28:47 +0800

    Penambahan bagian FAQ yang berisi pertanyaan umum Dogi For Us.

--- about.php
+++ about.php
@@ -130,5 +130,115 @@
     	</div>
     </section>
 
+     <section class="ftco-section bg-light ftco-faqs">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-lg-6 order-md-last">
+    				<div class="d-flex mt-3">
+    					<div class="img img-2 mr-md-2" style="background-image:url(images/about-2.jpg);"></div>
+    					<div class="img img-2 ml-md-2" style="background-image:url(images/about-3.jpg);"></div>
+    				</div>
+    			</div>
+
+    			<div class="col-lg-6">
+    				<div class="heading-section mb-5 mt-5 mt-lg-0">
+	            <h2 class="mb-3">Frequently Asks Questions</h2>
+	            <p>Many dog ​​lovers ask about Dogi For Us</p>
+    				</div>
+    				<div id="accordion" class="myaccordion w-100" aria-multiselectable="true">
+						  <div class="card">
+						    <div class="card-header p-0" id="headingOne">
+						      <h2 class="mb-0">
+						        <button href="#collapseOne" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="true" aria-controls="collapseOne">
+						        	<p class="mb-0">What is the process for becoming a community member and what are the requirements?</p>
+						          <i class="fa" aria-hidden="true"></i>
+						        </button>
+						      </h2>
+						    </div>
+						    <div class="collapse show" id="collapseOne" role="tabpanel" aria-labelledby="headingOne">
+						      <div class="card-body py-3 px-0">
+						      	<ol>
+						      		<li>You only need to fill out the online registration form on our website.</li>
+						      		<li>You can come directly to our open event and fill out the registration form there.</li>
+						      		<li>You only need to contact the available contacts and confirm your desire to become a member.</li>
+						      		<li>You will be asked to upload proof of dog ownership or a letter of recommendation from another member if you don't already own a dog.</li>
+						      		<li>The main requirement is to have a commitment to promoting dog welfare.</li>
+						      	</ol>
+						      </div>
+						    </div>
+						  </div>
+
+						  <div class="card">
+						    <div class="card-header p-0" id="headingTwo" role="tab">
+						      <h2 class="mb-0">
+						        <button href="#collapseTwo" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseTwo">
+						        	<p class="mb-0">What events or activities are usually held by the community?</p>
+						          <i class="fa" aria-hidden="true"></i>
+						        </button>
+						      </h2>
+						    </div>
+						    <div class="collapse" id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo">
+						      <div class="card-body py-3 px-0">
+						      	<ol>
+						      		<li>Dog Walks</li>
+						      		<li>Dog Training</li>
+						      		<li>Community Services</li>
+						      		<li>Seminars and Workshops</li>
+						      		<li>Social Gatherings</li>
+						      	</ol>
+						      </div>
+						    </div>
+						  </div>
+
+						  <div class="card">
+						    <div class="card-header p-0" id="headingThree" role="tab">
+						      <h2 class="mb-0">
+						        <button href="#collapseThree" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseThree">
+						        	<p class="mb-0">How does this community support education and training for dog owners?</p>
+						          <i class="fa" aria-hidden="true"></i>
+						        </button>
+						      </h2>
+						    </div>
+						    <div class="collapse" id="collapseThree" role="tabpanel" aria-labelledby="headingTwo">
+						      <div class="card-body py-3 px-0">
+						      	<ol>
+						      		<li>Our community regularly holds seminars and workshops led by professional dog grooming instructors and experts.</li>
+						      		<li>Our community partners with local dog training centers to offer training classes at special discounts for members.</li>
+						      		<li>Each month, we invite guest speakers who are experts in their fields to give presentations and question and answer sessions on current topics in the world of dogs.</li>
+						      		<li>We have an online resource library of articles, guides, video tutorials, and e-books that can be accessed by community members.</li>
+						      		<li>We have a mentor program where experienced dog owners volunteer to share their knowledge and advice with new members or novice dog owners.</li>
+						      	</ol>
+						      </div>
+						    </div>
+						  </div>
+
+						  <div class="card">
+						    <div class="card-header p-0" id="headingFour" role="tab">
+						      <h2 class="mb-0">
+						        <button href="#collapseFour" class="d-flex py-3 px-4 align-items-center justify-content-between btn btn-link" data-parent="#accordion" data-toggle="collapse" aria-expanded="false" aria-controls="collapseFour">
+						        	<p class="mb-0">Does this community have any affiliations or partnerships with other dog organizations?</p>
+						          <i class="fa" aria-hidden="true"></i>
+						        </button>
+						      </h2>
+						    </div>
+								<div class="collapse" id="collapseFour" role="tabpanel" aria-labelledby="headingThree">
+								<div class="card-body py-3 px-0">
+									<ol>
+										<li>Our community has close partnerships with several local dog organizations.</li>
+										<li>Our community does not yet have any official affiliation with any other dog organizations.</li>
+										<li>We have a partnership with the local Association of Professional Dog Trainers.</li>
+										<li>Our community is affiliated with the National Rescue Dog Network.</li>
+										<li>Our community is focused on building our own member base and internal programs.</li>
+									</ol>
+								</div>
+							</div>
+						</div>
+	        </div>
+        </div>
+    	</div>
+    </section>
+<!-- END ABOUT US PAGE -->
+
+
 </body>
 </html>
