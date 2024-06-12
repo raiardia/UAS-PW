05b31ff92984184a5b259ef2c0034e0937dbfacd
raiardia <ardiaanggreni@gmail.com>
2024-06-12 08:02:27 +0800

    Menambahkan navigasi untuk halaman quiz pada navbar

--- hasil_quiz.php
+++ hasil_quiz.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>UAS PEMROGRAMAN WEB</title>
+
 </head>
 <body>
+    <?php include "layout/header" ?>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	    	<a class="navbar-brand" href="index.php"><span class="flaticon-pawprint-1 mr-2"></span>DOGI FOR US</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+				<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+				<li class="nav-item"><a href="about.php" class="nav-link">About Us</a></li>
+				<li class="nav-item"><a href="gallery.php" class="nav-link">Gallery</a></li>
+				<li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+				<li class="nav-item active"><a href="quiz.php" class="nav-link">Quiz</a></li>
+			</ul>
+	      </div>
+	    </div>
+	  </nav>
+<!-- END nav -->
     
 </body>
 </html>
